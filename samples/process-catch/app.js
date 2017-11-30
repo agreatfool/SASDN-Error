9f3f33848506ae86718b5f1ad5ff9ce412e78c17
Jonathan Dai <nightghost5078@gmail.com>
2017-11-30 15:25:22 +0800

    Enhance sample

--- samples/process-catch/app.js
+++ samples/process-catch/app.js
@@ -1,31 +1,32 @@
 // 捕获 error
 process.on('uncaughtException',  (error)  =>  {
-    console.log('uncaughtException error = ', error);
-})
+    console.log('process on uncaughtException error = ', error);
+});
+
 // 捕获 reject   
 process.on('unhandledRejection',  (error)  =>  {
-    console.log('unhandledRejection error = ', error);
-})
+    console.log('process on unhandledRejection error = ', error);
+});
 
 async function throwException() {
     try {
         setTimeout(() => {
-            throw new Error('error')
-        })
+            throw new Error('error from function throwException');
+        }, 1);
     } catch (error) {
-        console.log('catch error = ', error);  // never catch
+        console.log('caught error in throwException: ', error); // never caught
     }
 }
 
 async function throwRejection() {
     try {
        setTimeout(() => {
-           Promise.reject('reject');
-       }) 
+           return Promise.reject('rejected from function throwRejection');
+       }, 1);
     } catch (error) {
-        console.log('catch error = ', error);  // never catch
+        console.log('caught error in throwRejection: ', error);  // never caught
     }
 }
 
-throwException();
-throwRejection();
+throwException().then(_ => _);
+throwRejection().then(_ => _);
